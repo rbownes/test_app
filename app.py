8baec8e3fddb705ef7c563f35444820c2a9bcf20
rbownes <richard.bownes@bbc.co.uk>
2021-03-02 22:25:40 +0000

    Fixed the problems, kind of, atleast cleaned it up.

--- app.py
+++ app.py
@@ -6,39 +6,6 @@ import random
 
 from acquire.grabques import *
 
-from operations.question import askQuestion
-import random
-from typing import Optional
-from fastapi import FastAPI, Request
-
-
-app = FastAPI()
-
-question = add_problem(1)
-operators = {"Addition":"+",
-             "Subtraction":"-", 
-             "Multiplication":"*", 
-             "Division":"/"}
-
-@app.get("/Random")
-def read_root():
-    return question.create_random_problem()
-
-@app.get("/{arithmatic}")
-def read_root(arithmatic: str):
-    if arithmatic == "Operator":
-        sign = "Operator"
-    else:
-        sign = operators.get(arithmatic)
-    
-    return question.create_arithmatic_problem(sign)
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info")
-
-
 st.write("""
 # My First App
 """)
