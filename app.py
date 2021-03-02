b9af6a3573298f79263fec29c74e99d4f803374a
rbownes <richard.bownes@bbc.co.uk>
2021-03-02 13:53:27 +0000

    really reaching here

--- app.py
+++ app.py
@@ -33,6 +33,12 @@ def read_root(arithmatic: str):
     
     return question.create_arithmatic_problem(sign)
 
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info")
+
+
 st.write("""
 # My First App
 """)
